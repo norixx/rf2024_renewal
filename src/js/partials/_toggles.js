705793a55c0c044e308200b9960a3e2f14c4632c
noir2tea <nori2tae@gmail.com>
2024-03-04 18:07:17 +0900

    フッター開発中

--- src/js/partials/_toggles.js
+++ src/js/partials/_toggles.js
@@ -1,10 +1,14 @@
 import { rf_get_globals, rf_set_globals } from './_globals'
 
 const rf_toggles = () => {
-  const toggles = document.querySelectorAll('[data-toggle]')
+  const toggles = document.querySelectorAll('[data-js-toggle]')
+  const toggleSPClass = 'toggle-sp-only'
+  const togglePCClass = 'toggle-pc-only'
   const toggleClass = '--is-open'
+
+  console.dir(toggles)
   toggles.forEach(toggle => {
-    const target = document.querySelector(toggle.dataset.toggle)
+    const target = document.querySelector(toggle.dataset.jsToggle)
     // 初動
     if (toggle.classList.contains(toggleClass) && !target.classList.contains(toggleClass)) {
       target.classList.add(toggleClass)
@@ -13,10 +17,15 @@ const rf_toggles = () => {
     // レスポンシブチェック
     toggle.addEventListener('click', e => {
       const self = e.target
-      if (self.classList.contains('toggle-sp-only') && !rf_get_globals('window_state') === 'SP') {
+
+      if (self.classList.contains(toggleSPClass) && rf_get_globals('window_state') !== 'SP') {
         return
       }
-      const target = document.querySelector(toggle.dataset.toggle)
+      if (self.classList.contains(togglePCClass) && rf_get_globals('window_state') !== 'PC') {
+        return
+      }
+      self.classList.toggle(toggleClass)
+      // const target = document.querySelector(toggle.dataset.toggle)
       target.classList.toggle(toggleClass)
     })
   })
--- src/js/rf.js
+++ src/js/rf.js
@@ -1,11 +1,14 @@
-import rf_globals from './partials/_globals';
+// import rf_globals from './partials/_globals';
 import rf_responsive_check from "./partials/_responsive";
 import rf_toggles from "./partials/_toggles";
 
 // =======================================================
 // Inits
+
+// Viewport checker (SP or PC)
 rf_responsive_check()
 
-if (document.querySelector('[data-toggle]')) {
+// Toggles
+if (document.querySelector('[data-js-toggle]')) {
   rf_toggles()
 }
