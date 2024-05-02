f6374dd3d2dfd9b9a2fee86830b8053220126f91
noir2tea <nori2tae@gmail.com>
2024-05-02 17:20:48 +0900

    added toggle mutation observer

--- src/js/partials/_toggles.js
+++ src/js/partials/_toggles.js
@@ -1,60 +1,177 @@
 // Toggle 共通
 import { rf_get_globals } from './_global-setting'
 
+// TODO: AJAXロードされるtoggleもあるので対策する
 const rf_toggles = () => {
   const toggles = document.querySelectorAll('[data-js-toggle]')
+  const toggleSelector = 'data-js-toggle'
+  const toggleTextSelector = 'data-js-toggle-text'
+  const html = document.documentElement
   const toggleSPClass = 'toggle-sp-only'
   const togglePCClass = 'toggle-pc-only'
   const toggleClass = '--is-open'
+  let mutationControl
+  const mutationConfig = {
+    childList: true, //対象ノードの子ノードに対する追加・削除の監視を有効に
+    subtree: true, //対象ノードとその子孫ノードに対する変更の監視を有効に
+    // attributes: true, //対象ノードの属性に対する変更の監視を有効に
+    // characterData: true, //対象ノードのテキストデータの変更の監視を有効に
+  };
 
-  const setToggleControl = () => {
-    toggles.forEach(toggle => {
-      const target = document.querySelector(toggle.dataset.jsToggle)
-      let toggle_text
-      // テキスト変更がある場合
-      if (toggle.hasAttribute('data-js-toggle-text')) {
-        toggle_text = toggle.dataset.jsToggleText
-      }
+  // テキスト変更のあるトグルの設定
+  const setToggleTextAndClass = toggle => {
+    const toggleTarget = document.querySelector(toggle.dataset.jsToggle)
 
-      // 初動 - トグルボタンに--is-openがついていればターゲットにも追加
-      if (toggle.classList.contains(toggleClass) && !target.classList.contains(toggleClass)) {
-        target.classList.add(toggleClass)
-        toggle.setAttribute('data-js-toggle-text', toggle.textContent)
-        toggle.textContent = toggle_text
-      }
+    //トグルボタンに--is-openがついていればターゲットにも追加
+    if (toggle.classList.contains(toggleClass) && !toggleTarget.classList.contains(toggleClass)) {
+      toggleTarget.classList.add(toggleClass)
+      toggle.setAttribute('data-js-toggle-text', toggle.textContent)
+      toggle.textContent = toggle_text
+    }
 
+    // テキスト変更がある場合
+    if (!toggle.hasAttribute(toggleTextSelector)) return
+    let toggle_text
+    // テキスト変更がある場合
+    if (toggle.hasAttribute(toggleTextSelector)) {
+      toggle_text = toggle.dataset.jsToggleText
+    }
+  }
 
-      // レスポンシブチェック
-      toggle.addEventListener('click', e => {
-        // const self = e.target
-        // SP
-        if (toggle.classList.contains(toggleSPClass) && rf_get_globals('window_state') !== 'SP') {
-          return
-        }
-        // PC
-        if (toggle.classList.contains(togglePCClass) && rf_get_globals('window_state') !== 'PC') {
-          return
-        }
+  // ミューテーションオブザーバー
+  const setToggleText = () => {
+    window.addEventListener('load', () => {
+      mutationControl = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          mutation.addedNodes.forEach(node => {
+            let toggles = []
+            if (node.hasAttribute(toggleSelector)) {
+              toggles.push(node)
+            }
+            if (node.querySelector(toggleSelector)) {
+              toggles.push(Array.from(node.querySelectorAll(toggleSelector)))
+            }
 
-        // トグルボタンのテキスト切り替え、クラス切り替え
-        if (toggle.hasAttribute('data-js-toggle-text')) {
-          toggle_text = toggle.getAttribute('data-js-toggle-text')
-          console.log(toggle_text)
-          toggle.setAttribute('data-js-toggle-text', toggle.textContent)
-          toggle.textContent = toggle_text
-        }
-        toggle.classList.toggle(toggleClass)
-        toggle.setAttribute('aria-expanded', toggle.classList.contains(toggleClass))
+            if (!toggles) return
 
-        // ターゲットのクラス切り替え
-        target.classList.toggle(toggleClass)
-        target.setAttribute('aria-hidden', !toggle.classList.contains(toggleClass))
+            console.log(toggles)
+
+            toggles.forEach(toggle => {
+              setToggleTextAndClass(toggle)
+            })
+
+          })
+        })
       })
+
+      mutationControl.observe(document.documentElement, mutationConfig)
     })
   }
 
+  // トグルボタンとターゲットのクラス、属性の変更
+  const setToggleAttrAndText = toggle => {
+    //トグルターゲットの取得
+    const toggleTarget = document.querySelector(toggle.dataset.jsToggle)
+
+    // ターゲットのクラス,ARIA属性の切り替え
+    toggleTarget.classList.toggle(toggleClass)
+    toggleTarget.setAttribute('aria-hidden', !toggle.classList.contains(toggleClass))
+    toggle.classList.toggle(toggleClass)
+    toggle.setAttribute('aria-expanded', toggle.classList.contains(toggleClass))
+
+    // トグルボタンのテキスト切り替え、クラス切り替え
+    if (!toggle.hasAttribute('data-js-toggle-text')) return
+    let toggle_text = toggle.getAttribute('data-js-toggle-text')
+    console.log(toggle_text)
+    toggle.setAttribute('data-js-toggle-text', toggle.textContent)
+    toggle.textContent = toggle_text
+  }
+
+  // トグルボタンの初期化
+  const initToggleControl = () => {
+    // AJAX追加されるボタンもあるので、ボタン直接ではなくHTMLクリックに変更
+    html.addEventListener('click', e => {
+      const target = e.target
+      let toggle
+      if (target.hasAttribute(toggleSelector)) {
+        toggle = target
+      } else if (target.closest(`${[toggleSelector]}`)) {
+        toggle = target.closest(`${[toggleSelector]}`)
+      } else {
+        return
+      }
+
+      // テキスト変更 - 分離済み
+
+      // レスポンシブチェック - SPかPCでトグルを制御
+      // SP
+      if (toggle.classList.contains(toggleSPClass) && rf_get_globals('window_state') !== 'SP') {
+        return
+      }
+      // PC
+      if (toggle.classList.contains(togglePCClass) && rf_get_globals('window_state') !== 'PC') {
+        return
+      }
+
+      // クラス、属性、テキストの変更
+      setToggleAttrAndText(toggle)
+
+    })
+
+    // toggles.forEach(toggle => {
+    //   const target = document.querySelector(toggle.dataset.jsToggle)
+    //   let toggle_text
+    //   // テキスト変更がある場合
+    //   if (toggle.hasAttribute('data-js-toggle-text')) {
+    //     toggle_text = toggle.dataset.jsToggleText
+    //   }
+
+    //   // 初動 - トグルボタンに--is-openがついていればターゲットにも追加
+    //   if (toggle.classList.contains(toggleClass) && !target.classList.contains(toggleClass)) {
+    //     target.classList.add(toggleClass)
+    //     toggle.setAttribute('data-js-toggle-text', toggle.textContent)
+    //     toggle.textContent = toggle_text
+    //   }
+
+
+    //   // レスポンシブチェック
+    //   toggle.addEventListener('click', e => {
+    //     // const self = e.target
+    //     // SP
+    //     if (toggle.classList.contains(toggleSPClass) && rf_get_globals('window_state') !== 'SP') {
+    //       return
+    //     }
+    //     // PC
+    //     if (toggle.classList.contains(togglePCClass) && rf_get_globals('window_state') !== 'PC') {
+    //       return
+    //     }
+
+    //     // トグルボタンのテキスト切り替え、クラス切り替え
+    //     if (toggle.hasAttribute('data-js-toggle-text')) {
+    //       toggle_text = toggle.getAttribute('data-js-toggle-text')
+    //       console.log(toggle_text)
+    //       toggle.setAttribute('data-js-toggle-text', toggle.textContent)
+    //       toggle.textContent = toggle_text
+    //     }
+    //     toggle.classList.toggle(toggleClass)
+    //     toggle.setAttribute('aria-expanded', toggle.classList.contains(toggleClass))
+
+    //     // ターゲットのクラス,ARIA属性の切り替え
+    //     target.classList.toggle(toggleClass)
+    //     target.setAttribute('aria-hidden', !toggle.classList.contains(toggleClass))
+    //   })
+    // })
+  }
+
   const init = () => {
-    setToggleControl()
+    initToggleControl()
+    // テキスト変更
+    toggles.forEach(toggle => {
+      setToggleTextAndClass(toggle)
+    })
+
+    //ミューテーションオブザーバー - AJAXで追加されるコンテンツ対策
+    setToggleText()
   }
 
   init()
