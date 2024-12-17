585570145bb1441a1bff8866fcb5b8a892d74896
noir <nori2tae@gmail.com>
2024-12-17 18:38:37 +0900

    2024-12-17 update

---
+++ src/js/partials/_notion-config.js
@@ -0,0 +1,7 @@
+const config = {
+  NOTION_KEY : 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,
+  DB_NEW_ARRIVAL_ID :b82941815ff441278bb82e32e3a5759a,
+  DB_POPULAR_ID : xxxx
+}
+
+export default config
